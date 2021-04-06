da75e7308c5b2c255e57eb60eb13d2e1d1b0a66b
Juanes Quintero <juaneschrome@gmail.com>
2021-04-05 23:53:09 -0500

    order detail layout

--- app/views/home.py
+++ app/views/home.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template, flash, request
 from app.forms.shop_forms import OrderForm
 from app.utils.http_client import HttpClient
+from app.models import Order
+
 
 Home = Blueprint('Home', __name__)
 
 api_client = HttpClient.get_instance()
+
+product = dict(name='Xiaomi QiCYCLE', price=500, warranty=3)
 
 @Home.route('/', methods=('GET', 'POST'))
 def index():
@@ -13,10 +17,17 @@ def index():
     if request.method == 'POST':
         if form.validate_on_submit():
             flash(f'Check your order carefully before go to pay, {form.customer_name.data}! ', 'info')
-            product = dict(name='Xiaomi QiCYCLE', price=500, warranty=3)
         else:
             product = None
             flash('There is an error on the order form', 'danger')
         return render_template('resume.html', resume=form, product=product)
 
     return render_template('index.html', form=form)
+
+@Home.route('/status', methods=('GET'))
+def order_detail():
+    id = request.params.get('id')
+    order = Order().query.filter_by(id=id).first()
+    if not order:
+        flash('This order does not exist', 'danger')
+    return render_template('detail.html', order=order, product=product)

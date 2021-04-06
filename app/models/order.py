749b1d575821f7c72d14f51517503ccfbc8c3586
Juanes Quintero <jquintero@sistecredito.com>
2021-04-06 13:27:10 -0500

    order row on db

--- app/models/order.py
+++ app/models/order.py
@@ -4,12 +4,12 @@ from datetime import datetime
 
 class Order(db.Model):
 
-    id =  db.Column(db.Integer , primary_key=True , autoincrement=True)
+    id =  db.Column(db.Integer, primary_key=True, autoincrement=True)
     customer_name = db.Column(db.String(80), unique=False, nullable=False)
-    customer_email = db.Column(db.String(120), unique=True, nullable=False)
+    customer_email = db.Column(db.String(120), unique=False, nullable=False)
     customer_address = db.Column(db.String(200), unique=False, nullable=False)
-    customer_mobile = db.Column(db.String(40), unique=True, nullable=False)
-    status = db.Column(db.String(20), default='CREATED') # “CREATED, PAYED, REJECTED” 
+    customer_mobile = db.Column(db.String(40), unique=False, nullable=False)
+    status = db.Column(db.String(20), default='CREATED') # CREATED, PAYED, REJECTED 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=True, default=None)
 
